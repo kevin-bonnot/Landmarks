a35fb0c43315ba9210c2fe916dfb1092636b7e64
Renchglad <bonnot.kevin@gmail.com>
2025-02-10 17:17:33 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Renchglad on 10/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LandmarksTests/LandmarksTests.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksTests.swift
+//  LandmarksTests
+//
+//  Created by Renchglad on 10/02/2025.
+//
+
+import Testing
+@testable import Landmarks
+
+struct LandmarksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
